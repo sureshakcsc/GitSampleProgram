f989211de8608f8fa267ef79740bce08f18aaf17
DELL <DELL@192.168.1.4>
2021-08-05 18:15:02 +0530

    First commit for Google program

---
+++ src/main/java/google/GoogleTest.java
@@ -0,0 +1,22 @@
+package google;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GoogleTest {
+	
+	@Test
+	public void getdata()
+	{
+	
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.google.com");
+	driver.findElement(By.name("q")).sendKeys("jamesbond");
+	}
+
+}
